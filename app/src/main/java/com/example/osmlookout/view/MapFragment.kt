1d1bdd58128993f330f6ada891ca7ea23adf6706
“konstantin” <pon4ik2282211@gmail.com>
2023-10-02 19:23:20 +0300

    WIP

--- app/src/main/java/com/example/osmlookout/view/MapFragment.kt
+++ app/src/main/java/com/example/osmlookout/view/MapFragment.kt
@@ -7,10 +7,12 @@ import android.view.GestureDetector.OnGestureListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.osmlookout.databinding.FragmentMapBinding
 import com.example.osmlookout.vm.MapViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.osmdroid.config.Configuration
@@ -23,7 +25,7 @@ import org.osmdroid.views.overlay.infowindow.InfoWindow
 
 class MapFragment : Fragment() {
     private val viewModel: MapViewModel by viewModel()
-    private val binding by lazy{
+    private val binding by lazy {
         FragmentMapBinding.inflate(layoutInflater);
     }
 
@@ -32,19 +34,29 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val ctx = activity?.applicationContext
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+        //Bottom menu -> not draggable bottom sheet
+        BottomSheetBehavior.from(binding.standardBottomSheet).apply {
+            isDraggable = false
+            state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        binding.standardBottomSheet.isVisible = false
 
-        binding.map.setTileSource(TileSourceFactory.MAPNIK)
-        binding.map.setMultiTouchControls(true)
-        binding.map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
-        binding.map.overlayManager = CustomOverlayManager(binding.map, requireActivity())
+        activity?.applicationContext.let {
+            Configuration.getInstance().load(it, PreferenceManager.getDefaultSharedPreferences(it))
+        }
 
-        binding.map.controller.setZoom(18.0)
-        binding.map.controller.setCenter(GeoPoint(55.769687, 37.597566))
+        binding.map.run {
+            setTileSource(TileSourceFactory.MAPNIK)
+            setMultiTouchControls(true)
+            zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+            overlayManager = CustomOverlayManager(binding.map, requireActivity())
+
+            controller.setZoom(18.0)
+            controller.setCenter(GeoPoint(55.769687, 37.597566))
+        }
 
         lifecycleScope.launch {
-            viewModel.myPos.collect{
+            viewModel.myPos.collect {
                 addMarker(it)
             }
         }
@@ -54,18 +66,28 @@ class MapFragment : Fragment() {
 
     //                marker.icon = ContextCompat.getDrawable(requireActivity(),com.example.osmlookout.R.mipmap.ic_my_tracker_46dp)
 
-    private fun addMarker(point: GeoPoint){
+    private fun addMarker(point: GeoPoint) {
         val marker = CustomMarker(binding.map, requireActivity())
         marker.position = point
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         marker.infoWindow = object : InfoWindow(
             layoutInflater.inflate(com.example.osmlookout.R.layout.marker_info_window, null),
             binding.map
-        ){
+        ) {
             override fun onOpen(item: Any?) {}
             override fun onClose() {}
         }
-
+        marker.setOnMarkerClickListener { marker, mapView ->
+            marker.showInfoWindow()
+            onClickMarker(marker)
+            return@setOnMarkerClickListener true
+        }
         binding.map.overlays.add(marker)
     }
+
+    private fun onClickMarker(marker: Marker) {
+        binding.standardBottomSheet.isVisible = true
+    }
+
+
 }
