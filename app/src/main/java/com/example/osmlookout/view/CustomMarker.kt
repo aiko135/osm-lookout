15d972007463521fd1f7a628da500c351edc012c
“konstantin” <pon4ik2282211@gmail.com>
2023-10-02 17:26:55 +0300

    Marker

--- app/src/main/java/com/example/osmlookout/view/CustomMarker.kt
+++ app/src/main/java/com/example/osmlookout/view/CustomMarker.kt
@@ -12,57 +12,106 @@ import android.graphics.RectF
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
+import org.osmdroid.util.RectL
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
-//TODO maybe refactor to a fabric
-fun createCustomMarker(ctx:Context): Drawable {
-    val tracker =  ContextCompat.getDrawable(
-        ctx, com.example.osmlookout.R.mipmap.ic_tracker_75dp
-    )
-    val trackerBmp = (tracker as BitmapDrawable).bitmap
+class CustomMarker:Marker {
 
-    val photo =  ContextCompat.getDrawable(ctx, com.example.osmlookout.R.drawable.photo)
-    var photoBmp = (photo as BitmapDrawable).bitmap
-    // Apply SCALE of marker
-    photoBmp = Bitmap.createScaledBitmap(
-        photoBmp,
-        (trackerBmp.width * 0.6666).toInt(),
-        (trackerBmp.width * 0.6666).toInt(),
-        true
-    )
-    //Apply CORNER ROUNDING to make it circle shape
-    photoBmp = getRoundedCornerBitmap(photoBmp, photoBmp.width.toFloat())
+    constructor(map:MapView, ctx:Context) : super(map) {
+        this.icon = createCustomDrawable(ctx)
+    }
 
-    val resultBitmap = Bitmap.createBitmap(trackerBmp.width, trackerBmp.height, trackerBmp.config)
-    val canvas = Canvas(resultBitmap)
-    canvas.drawBitmap(trackerBmp, Matrix(), null)
-    val photoOffsetX = - canvas.width * 0.005
-    val photoOffsetY = - canvas.height * 0.0833
-    canvas.drawBitmap(
-        photoBmp,
-        (canvas.width / 2 - photoBmp.width / 2 + photoOffsetX).toFloat(), //offset
-        (canvas.height / 2 - photoBmp.height / 2 + photoOffsetY).toFloat(),
-        null
-    )
+    //Override to add an OFFSET to infoWindow view
+    override fun showInfoWindow() {
+        if (mInfoWindow == null)
+            return
+        val markerWidth = mIcon.intrinsicWidth
+        val markerHeight = mIcon.intrinsicHeight
+        //---- modified this ----------
+        var offsetX = (markerWidth * (mIWAnchorU - mAnchorU)).toInt()
+        offsetX += (markerWidth * 0.85).toInt()
+        var offsetY = - (markerHeight * (mIWAnchorV - mAnchorV)).toInt()
+        offsetY -= (markerHeight * 0.6).toInt()
+        //-----------------------------
+        if (mBearing == 0f) {
+            mInfoWindow.open(this, mPosition, offsetX, offsetY)
+            return
+        }
+        val centerX = 0
+        val centerY = 0
+        val radians = -mBearing * Math.PI / 180.0
+        val cos = Math.cos(radians)
+        val sin = Math.sin(radians)
+        val rotatedX = RectL.getRotatedX(
+            offsetX.toLong(),
+            offsetY.toLong(),
+            centerX.toLong(),
+            centerY.toLong(),
+            cos,
+            sin
+        ).toInt()
+        val rotatedY = RectL.getRotatedY(
+            offsetX.toLong(),
+            offsetY.toLong(),
+            centerX.toLong(),
+            centerY.toLong(),
+            cos,
+            sin
+        ).toInt()
+        mInfoWindow.open(this, mPosition, rotatedX, rotatedY)
+    }
 
-    return BitmapDrawable(ctx.resources, resultBitmap)
-}
+    private fun createCustomDrawable(ctx:Context): Drawable {
+        val tracker =  ContextCompat.getDrawable(
+            ctx, com.example.osmlookout.R.mipmap.ic_tracker_75dp
+        )
+        val trackerBmp = (tracker as BitmapDrawable).bitmap
 
-private fun getRoundedCornerBitmap(bitmap: Bitmap, roundPx:Float): Bitmap {
-    val output = Bitmap.createBitmap(
-        bitmap.width,
-        bitmap.height, Bitmap.Config.ARGB_8888
-    )
-    val canvas = Canvas(output)
-    val paint = Paint()
+        val photo =  ContextCompat.getDrawable(ctx, com.example.osmlookout.R.drawable.photo)
+        var photoBmp = (photo as BitmapDrawable).bitmap
+        // Apply SCALE of marker to image's bitmap
+        photoBmp = Bitmap.createScaledBitmap(
+            photoBmp,
+            (trackerBmp.width * 0.6666).toInt(),
+            (trackerBmp.width * 0.6666).toInt(),
+            true
+        )
+        //Apply CORNER ROUNDING to image's bitmap to make it circle shape
+        photoBmp = getRoundedCornerBitmap(photoBmp, photoBmp.width.toFloat())
 
-    val rect = Rect(0, 0, bitmap.width, bitmap.height)
-    val rectF = RectF(rect)
+        val resultBitmap = Bitmap.createBitmap(trackerBmp.width, trackerBmp.height, trackerBmp.config)
+        val canvas = Canvas(resultBitmap)
+        canvas.drawBitmap(trackerBmp, Matrix(), null)
+        val photoOffsetX = - canvas.width * 0.005
+        val photoOffsetY = - canvas.height * 0.0833
+        canvas.drawBitmap(
+            photoBmp,
+            (canvas.width / 2 - photoBmp.width / 2 + photoOffsetX).toFloat(), //offset
+            (canvas.height / 2 - photoBmp.height / 2 + photoOffsetY).toFloat(),
+            null
+        )
 
-    paint.isAntiAlias = true
-    canvas.drawARGB(0, 0, 0, 0)
-    paint.color = -0xbdbdbe
-    canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
-    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-    canvas.drawBitmap(bitmap, rect, rect, paint)
-    return output
+        return BitmapDrawable(ctx.resources, resultBitmap)
+    }
+
+    private fun getRoundedCornerBitmap(bitmap: Bitmap, roundPx:Float): Bitmap {
+        val output = Bitmap.createBitmap(
+            bitmap.width,
+            bitmap.height, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(output)
+        val paint = Paint()
+
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
+        val rectF = RectF(rect)
+
+        paint.isAntiAlias = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = -0xbdbdbe
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(bitmap, rect, rect, paint)
+        return output
+    }
 }
--- app/src/main/java/com/example/osmlookout/view/MapFragment.kt
+++ app/src/main/java/com/example/osmlookout/view/MapFragment.kt
@@ -18,6 +18,7 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.infowindow.InfoWindow
 
 
 class MapFragment : Fragment() {
@@ -44,15 +45,27 @@ class MapFragment : Fragment() {
 
         lifecycleScope.launch {
             viewModel.myPos.collect{
-                val marker = Marker(binding.map)
-                marker.position = it
-//                marker.icon = ContextCompat.getDrawable(requireActivity(),com.example.osmlookout.R.mipmap.ic_my_tracker_46dp)
-                marker.icon = createCustomMarker(requireActivity())
-                binding.map.overlays.add(marker)
+                addMarker(it)
             }
         }
 
         return binding.root
     }
 
+    //                marker.icon = ContextCompat.getDrawable(requireActivity(),com.example.osmlookout.R.mipmap.ic_my_tracker_46dp)
+
+    private fun addMarker(point: GeoPoint){
+        val marker = CustomMarker(binding.map, requireActivity())
+        marker.position = point
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.infoWindow = object : InfoWindow(
+            layoutInflater.inflate(com.example.osmlookout.R.layout.marker_info_window, null),
+            binding.map
+        ){
+            override fun onOpen(item: Any?) {}
+            override fun onClose() {}
+        }
+
+        binding.map.overlays.add(marker)
+    }
 }
